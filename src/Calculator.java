cd6f0e8a2320aee19b34bc5b7f6b0780337cdd96
King Wong <wong.kinghang1997@gmail.com>
2022-06-14 11:51:57 +0100

    Initial Commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int dividion(int num1, int num2){
+        return num1 / num2;
+    }
+    public static void main(String[] args){
+         //int num1 = 1;
+         //int num2 = 2;
+       // int total = add(1,2);
+       //  System.out.println(total);
+    }
+}
